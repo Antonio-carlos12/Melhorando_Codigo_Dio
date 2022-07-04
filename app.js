b154e86322b0f7b4df7b02ca7a0ac6bdaa906e71
Antonio <antoniocarlosdesouzaj@gmail.com>
2022-07-04 12:20:17 -0300

    Melhorado

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+//Codigo a melhorar{
+//let employee = {};
+// employee.code = 10;
+//  employee.name = "John";
+//}
+const trabalhador = {
+    name: "Antonio",
+    code: 20,
+    empresa: "Liferay"
+};
---
+++ app.ts
@@ -0,0 +1,17 @@
+//Codigo a melhorar{
+    //let employee = {};
+   // employee.code = 10;
+  //  employee.name = "John";
+//}
+
+type Employee = {
+    code: Number,
+    name: String,
+    empresa?: String
+}
+
+const trabalhador: Employee ={
+    name: "Antonio",
+    code: 20,
+    empresa :"Liferay" 
+}
